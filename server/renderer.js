ee834b0513ba3cbf69be864d75732642d0d01de7
i038849 <ashish.rawat@sap.com>
2017-11-06 14:46:38 +0530

    merged chagnes

--- server/renderer.js
+++ server/renderer.js
@@ -2,10 +2,7 @@ var express = require("express");
 var cors = require("cors");
 var path = require("path");
 var bodyParser = require("body-parser");
-<<<<<<< HEAD
 var mongoconnector = require("./mongodbconnector");
-=======
->>>>>>> 1abda33d06d35df0ab29103ff1719bcc4f290171
 
 var app = new express();
 var expressWS = require('express-ws')(app);
@@ -14,21 +11,13 @@ app.use(cors());
 app.use(function (req, res, next) {
     console.log(req.url);
     console.log(req.method);
-<<<<<<< HEAD
    
     mongoconnector.connectMongodb();
-=======
-    //console.log(req);
->>>>>>> 1abda33d06d35df0ab29103ff1719bcc4f290171
     next();
 
 });
 
 app.use(express.static('../app/dist')); 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1abda33d06d35df0ab29103ff1719bcc4f290171
 //app.use('/static', express.static(path.join(__dirname,'../app/dist/static')));
 /*app.get('/', function (req, res) {
     console.log('root:' + req.url);
@@ -59,16 +48,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
 app.post('/Events', function (req, res) {
     console.log(req.body);
     
     mongoconnector.insertEvent(req.body);
-=======
-app.post('/app/dist/index.html', function (req, res) {
-    console.log(req.body.fname);
-    console.log(req.body.message);
->>>>>>> 1abda33d06d35df0ab29103ff1719bcc4f290171
     // res.end("sucessfully added");
 
 });
